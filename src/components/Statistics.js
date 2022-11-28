4f74b1e22765d948ad5a0052d5ee37a0bbfb678d
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-11-28 15:06:29 +0200

    Update Statistics.js

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,3 +1,33 @@
-export const Statistics = () => {
-    
+import PropTypes from 'prop-types';
+import { rendomColor } from 'utils/RandomColor';
+
+import {Section, Title, DataList, DataItem, Label, Percentage  } from './Statistics.styled';
+
+export const Statistics = ({ title, data }) => {
+  return (
+    <Section>
+      {title && <Title>{title}</Title>}
+
+      <DataList>
+        {data.map(({ id, label, percentage }) => (
+          <DataItem bgcolor={rendomColor()} key={id}>
+            <Label>{label}</Label>
+            <Percentage>{percentage}%</Percentage>
+          </DataItem>
+        ))}
+      </DataList>
+    </Section>
+  );
 };
+
+
+Statistics.propTypes = {
+title: PropTypes.string,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+}
