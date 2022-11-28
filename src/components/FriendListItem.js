4d6a92f86a76562957bd992666bc272dda0148a3
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-11-28 16:36:12 +0200

    Create FriendListItem

---
+++ src/components/FriendListItem.js
@@ -0,0 +1,12 @@
+import { Friend, Label, AvatarImg, Name } from './FriendListItem.styled';
+
+
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+    return (
+        <Friend>
+            <Label bgColor={isOnline ? '#52ad56' : '#ff4b58'}></Label>
+            <AvatarImg alt={name} src={avatar} width="60" height="60" />
+            <Name> {name}</Name>
+        </Friend>
+    );
+};
