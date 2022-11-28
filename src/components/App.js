cda6bd28f954875285d855dc8672e9e10a1efec4
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-11-28 16:35:35 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
+import { GlobalStyle } from './Global.styled'
+
 import { Profile } from "./Profile";
 import { Statistics } from "./Statistics";
+import { FriendList } from "./FriendList";
+import { TransactionHistory } from "./TransactionHistory";
 
-import user from "../../src/data/user.json"
+import user from "../../src/data/user.json";
+import data from "../../src/data/data.json";
+import friends from "../../src/data/friends.json"
+import transactions from "../../src/data/transactions.json"
 
 
 export const App = () => {
   return (
-    <div>
-      <Profile user={user[0]} />
-      <Statistics/>
-    </div>
+    <>
+      <GlobalStyle />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats} />
+      <Statistics title="Upload stats" data={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
+    </>
   );
 };
